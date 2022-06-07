d8bcd4f6c299606550a303d225b5e7d637d8ce9d
KrystianKiznar <krystian.kiznar@gmail.com>
2022-06-07 16:59:58 +0200

    added level 0 (addition, subtraction, multiplication and division)

--- NoSoSiCa/main.cpp
+++ NoSoSiCa/main.cpp
@@ -6,6 +6,41 @@ stack<char> my_stack;
 string operators{ "+-*/" };
 
 
+//bool IsDigit(char element)
+//{
+//	return element >= '0' && element <= '9';
+//}
+//
+//int str_to_int(string a, int& poz)
+//{
+//	int liczba = 0;
+//	while (poz < a.size() && IsDigit(a[poz]))
+//	{
+//		//schemat Hornera
+//		liczba = liczba * 10 + a[poz] - '0';
+//		++poz;
+//	}
+//	--poz;
+//	return liczba;
+//}
+
+int MakeCalculation(int a, int b, char oper)
+{
+	switch (oper)
+	{
+	case '+':
+		return a + b;
+	case '-':
+		return a - b;
+	case '*':
+		return a * b;
+	case '/':
+		return a / b;//dzielenie calkowite
+	}
+	cout << "Unexpected sign!";
+	return 0;
+}
+
 string ConvertOperationIntoNotation(string operation) {
 	//takes operatnion and convert it into RPN notation. A+B*C-D ==> ABC*+D- 
 	string notation{};
@@ -39,12 +74,38 @@ string ConvertOperationIntoNotation(string operation) {
 	return notation;
 }
 
+int Calculate(string notation) {
+	int result;
+
+	for (auto x : notation) {
+		int value_a, value_b;
+		if (x >= '0' && x <= '9')
+			my_stack.push(x - 48);
+
+		else {
+			int out;
+			value_b = my_stack.top();
+			my_stack.pop();
+			value_a = my_stack.top();
+			my_stack.pop();
+
+			my_stack.push(MakeCalculation(value_a, value_b, x));
+		}
+	}
+	return my_stack.top();
+}
+
+
 int main() {
 
-	string operation{ "4*4+3/2" };
+	string operation{ "4*4+3/3" };
 	string notation{};
+	int result;
 
 	notation = ConvertOperationIntoNotation(operation);
+	result = Calculate(notation);
 	cout << "For " << operation << " notation is: " << notation << endl;
+	cout << "For this " << operation << " the output is " << result << endl;
+
 	return 0;
 }
